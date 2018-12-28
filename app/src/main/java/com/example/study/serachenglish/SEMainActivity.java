1197294d467e2675a5871aee12d8c536a88f6c01
Hou.Yi <houyisi4797@sogou-inc.com>
2018-12-28 10:03:20 +0800

    first commit

---
+++ app/src/main/java/com/example/study/serachenglish/SEMainActivity.java
@@ -0,0 +1,13 @@
+package com.example.study.serachenglish;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SEMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_semain);
+    }
+}
